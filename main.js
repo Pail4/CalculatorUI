7c8bd0adc8de432f2ee5054c9e267053d71a1772
Pail4 <73517684+Pail4@users.noreply.github.com>
2021-11-27 19:21:05 +0400

    its work...mostly

--- main.js
+++ main.js
@@ -8,32 +8,77 @@ function calc(operation, a, b) {
             return a * b;
         case 'div':
             return a / b;
+        default:
+            return a;
     }
 }
-
+let firstNumber = 0;
 let currentNumber = 0;
+let operation = 'sum';
+
+let deleteBtn = document.querySelector(".delete");
+let clear = document.querySelector(".clear");
+let line = document.querySelector(".line");
+let numbers = document.querySelectorAll(".num");
+let operations = document.querySelectorAll(".operation")
 
 function numberClick() {
-    currentNumber += this.textContent;
+    if (currentNumber < 10000) {
+        currentNumber += this.textContent;
+        update();
+    }
+
 }
 
 function operatorClick() {
-
+    getResult(operation, firstNumber, currentNumber)
+    operation = this.id;
+    firstNumber = currentNumber;
+    currentNumber = 0;
 }
 
 function getResult() {
-
+    currentNumber = calc(operation, firstNumber, currentNumber);
+    update();
+    if (isNaN(currentNumber)){
+        setTimeout(() => {
+            clearLine();
+        }, 1000);
+    }
 }
 
-function clear() {
-
+function clearLine() {
+    firstNumber = 0;
+    currentNumber = 0;
+    operation = "sum";
+    update();
 }
 
 function undo() {
-
+    currentNumber = Math.trunc(currentNumber / 10);
+    update();
 }
 
-window.onload = function() {
-    let line = document.querySelector(".line");
+function update() {
+    currentNumber = Number(currentNumber);
+    if (currentNumber >= 100000) {
+        line.style.overflow = "auto";
+        line.style.fontSize = "74px";
+    }
+    else if (line.style.overflow === "auto"){
+        line.style.overflow = "inherit";
+        line.style.fontSize = "96px";
+    }
     line.textContent = currentNumber;
+}
+
+window.onload = function () {
+    for (let digit of numbers) {
+        digit.onclick = numberClick;
+    }
+    for (let op of operations) {
+        op.onclick = operatorClick;
+    }
+    clear.onclick = clearLine;
+    deleteBtn.onclick = undo;
 }
